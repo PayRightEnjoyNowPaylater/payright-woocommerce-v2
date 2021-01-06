2618ae56f3ab0bf08b17fd2f56d3e79e3e15ba63
Brian Ng <brian.ng@payright.com.au>
2021-01-06 22:58:22 +0000

    Update API endpoints for e-commerce v2

--- woocommerce/includes/config.php
+++ woocommerce/includes/config.php
@@ -5,9 +5,9 @@ $sandbox       = $theme_options['sandbox'];
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
 
 if (strtolower($sandbox) == "yes") {
-    define("PAYRIGHT_APIENDPOINT", "https://byronbay-dev.payright.com.au/");
+    define("PAYRIGHT_APIENDPOINT", "https://sandbox.payright.com/au/checkout/");
 
 } else {
-    define("PAYRIGHT_APIENDPOINT", "https://byronbay-dev.payright.com.au/");
+    define("PAYRIGHT_APIENDPOINT", "https://api.payright.com/au/checkout/");
 }
 
